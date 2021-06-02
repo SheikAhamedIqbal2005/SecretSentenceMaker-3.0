979b1888e593137d3c710b1816129fc297fb946a
SheikAhamedIqbal2005 <76652186+SheikAhamedIqbal2005@users.noreply.github.com>
2021-06-02 19:24:41 +0530

    Add files via upload

---
+++ Decryptor/Files/script.js
@@ -0,0 +1,86 @@
+function converter () {
+
+   var secretsent = document.getElementById("normalsentence").value;
+   var answer = document.getElementById("Answer")
+
+   var a = secretsent.replace(/P229I/g, "a");
+   secretsent = a;
+
+   var b = secretsent.replace(/JFAVO/g, "b");
+   secretsent = b;
+
+   var c = secretsent.replace(/1KV54/g, "c");
+   secretsent = c;
+
+   var d = secretsent.replace(/KNS67/g, "d");
+   secretsent = d;
+
+   var e = secretsent.replace(/5NM4T/g, "e");
+   secretsent = e;
+
+   var f = secretsent.replace(/M7ZOZ/g, "f");
+   secretsent = f;
+
+   var g = secretsent.replace(/5BMOS/g, "g");
+   secretsent = g;
+
+   var h = secretsent.replace(/RL5FJ/g, "h");
+   secretsent = h;
+
+   var i = secretsent.replace(/KM40I/g, "i");
+   secretsent = i;
+
+   var j = secretsent.replace(/EGXPJ/g, "j");
+   secretsent = j;
+
+   var k = secretsent.replace(/MCB0H/g, "k");
+   secretsent = k;
+
+   var l = secretsent.replace(/OIT4W/g, "l");
+   secretsent = l;
+
+   var m = secretsent.replace(/H9OI0/g, "m");
+   secretsent = m;
+
+   var n = secretsent.replace(/USWTU/g, "n");
+   secretsent = n;
+
+   var o = secretsent.replace(/9PEWA/g, "o");
+   secretsent = o
+
+   var p = secretsent.replace(/1FF3Y/g, "p");
+   secretsent = p;
+
+   var q = secretsent.replace(/E0NXQ/g, "q");
+   secretsent = q;
+
+   var r = secretsent.replace(/9ORMB/g, "r");
+   secretsent = r;
+
+   var s = secretsent.replace(/MNK59/g, "s");
+   secretsent = s;
+  
+   var t = secretsent.replace(/HZKWJ/g, "t");
+   secretsent = t;
+
+   var u = secretsent.replace(/GDAAH/g, "u");
+   secretsent = u;
+
+   var v = secretsent.replace(/STIVK/g, "v");
+   secretsent = v;
+
+   var w = secretsent.replace(/6FUFQ/g, "w");
+   secretsent = w;
+
+   var x = secretsent.replace(/EEFXW/g, "x");
+   secretsent = x;
+
+   var y = secretsent.replace(/UMIUH/g, "y");
+   secretsent = y;
+
+   var z = secretsent.replace(/4MPWX/g, "z");
+   secretsent = z;
+
+   Answer.innerHTML = secretsent;
+
+         }
---
+++ Encryptor/Files/script.js
@@ -0,0 +1,141 @@
+function converter() {
+
+   var secretsent = document.getElementById("normalsentence").value;
+   var answer = document.getElementById("Answer")
+
+   if (secretsent == "a") {
+		  answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "b") {
+    	  answer.innerHTML = "Please Input a Valid Sentence"      
+   } else if (secretsent == "c") {
+   	      answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "d") {
+    	  answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "e") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "f") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "g") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "h") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "i") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "j") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "k") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "l") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "m") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "n") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "o") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "p") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "q") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "r") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "s") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "t") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "u") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "v") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "w") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "x") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "y") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "z") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else if (secretsent == "abcdefghijklmnopqrstuvwxyz") {
+          answer.innerHTML = "Please Input a Valid Sentence"
+   } else {
+		  var a = secretsent.replace(/a/g,"P229I");
+          secretsent = a;
+
+          var b = secretsent.replace(/b/g, "JFAVO");
+          secretsent = b;
+ 
+          var c = secretsent.replace(/c/g, "1KV54");
+          secretsent = c;
+
+          var d = secretsent.replace(/d/g, "KNS67");
+          secretsent = d;
+
+          var e = secretsent.replace(/e/g, "5NM4T");
+          secretsent = e;
+
+          var f = secretsent.replace(/f/g, "M7ZOZ");
+          secretsent = f;
+
+          var g = secretsent.replace(/g/g, "5BMOS");
+          secretsent = g;
+
+          var h = secretsent.replace(/h/g, "RL5FJ");
+          secretsent = h;
+
+          var i = secretsent.replace(/i/g, "KM40I");
+          secretsent = i;
+
+          var j = secretsent.replace(/j/g, "EGXPJ");
+          secretsent = j;
+
+          var k = secretsent.replace(/k/g, "MCB0H");
+          secretsent = k;
+
+          var l = secretsent.replace(/l/g, "OIT4W");
+          secretsent = l;
+
+          var m = secretsent.replace(/m/g, "H9OI0");
+          secretsent = m;
+
+          var n = secretsent.replace(/n/g, "USWTU");
+          secretsent = n;
+
+          var o = secretsent.replace(/o/g, "9PEWA");
+          secretsent = o;
+
+          var p = secretsent.replace(/p/g, "1FF3Y");
+          secretsent = p;
+
+          var q = secretsent.replace(/q/g, "E0NXQ");
+          secretsent = q;
+
+          var r = secretsent.replace(/r/g, "9ORMB");
+          secretsent = r;
+
+          var s = secretsent.replace(/s/g, "MNK59");
+          secretsent = s;
+
+          var t = secretsent.replace(/t/g, "HZKWJ");
+          secretsent = t;
+
+          var u = secretsent.replace(/u/g, "GDAAH");
+          secretsent = u;
+       
+          var v = secretsent.replace(/v/g, "STIVK")
+          secretsent = v;
+
+          var w = secretsent.replace(/w/g, "6FUFQ");
+          secretsent = w;
+
+          var x = secretsent.replace(/x/g, "EEFXW");
+          secretsent = x;
+
+          var y = secretsent.replace(/y/g, "UMIUH");
+          secretsent = y;
+
+          var z = secretsent.replace(/z/g, "4MPWX");
+          secretsent = z;
+          
+          Answer.innerHTML = secretsent;          
+          }
+}
